5787e137f88d1f79b541d13546f3f4c0091e917f
caroleguardiola <carole.guardiola@gmail.com>
2017-11-21 17:23:18 +0100

    Theme customized

---
+++ blog_post_detail.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Sept - StratUp Landing Page</title>
+		
+		<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/font-awesome.min.css" rel="stylesheet">
+
+
+		<link href="css/owl.carousel.css" rel="stylesheet">
+		<link href="css/owl.theme.css" rel="stylesheet">
+		<link href="css/owl.transitions.css" rel="stylesheet">
+
+		<link href="css/style.css" rel="stylesheet">
+
+
+
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="js/html5shiv.min.js"></script>
+		<script src="js/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body data-spy="scroll" data-target=".main-nav">
+
+		<header class="st-navbar">
+			<nav class="navbar navbar-default navbar-fixed-top clearfix" role="navigation">
+				<div class="container"><!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sept-main-nav">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Carole Guardiola</a>
+					</div>
+					
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse main-nav" id="sept-main-nav">
+						<ul class="nav navbar-nav navbar-right">
+							<li class="active"><a href="index.php">Accueil</a></li>
+							<li class="dropdown">
+				              <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="javascript:;">
+				                Blog 
+				              </a>
+					              <ul class="dropdown-menu">
+					              	<li><a href="blog_post.php">Blog</a></li>
+					                <li><a href="blog_post_creation.php">Création blog post</a></li>
+					              </ul>
+				            </li>
+							<li><a href="index.php">Contact</a></li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div>
+			</nav>
+		</header>
+		
+		<section class="home_blog" id="home">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="st-home-unit-blog">
+							<div class="hero-txt">
+								<p class="hero-work">Web Design - Web Development - UX Design</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="blog" id="blog">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8">
+						<div class="blog-post">
+							<h2 class="post-title"><a href="#">A example blog post with image</a></h2>
+							<div class="post-meta">
+								<span class="author"><a href="#"> Raju Ahmmed</a></span>,
+								<span>Posted in <a href="#">WordPress</a></span>,
+								<span>At <strong>10 January, 2015</strong></span>
+							</div>
+							
+							<div class="post-content">
+								<p>Torquatos nihil utuntur liberamur benivole epularum evertunt accommodare, democrito didicerimus occulta egregios, praeclaram familiaritatem novi accusamus delectus opinemur mutae expetenda perpauca eas, concederetur fecerit ignorare delectari industria patientiamque. Invenire probes contereret maior opinor iucundo inciderint depravate, concupiscunt, deorum didicerimus, solitudo ingeniis confectum attento illustribus. Quanta suam, quieti usu necessarius. Sapienter existimo fruenda contentiones deduceret dolere minuti animi scientia, inbecilloque servire evertunt. Exeduntur vivi laudatum dicitur omnia verterem fuerit inanitate efficiatur</p>
+								<p>Torquatos nihil utuntur liberamur benivole epularum evertunt accommodare, democrito didicerimus occulta egregios, praeclaram familiaritatem novi accusamus delectus opinemur mutae expetenda perpauca eas, concederetur fecerit ignorare delectari industria patientiamque. Invenire probes contereret maior opinor iucundo inciderint depravate, concupiscunt, deorum didicerimus, solitudo ingeniis confectum attento illustribus. Quanta suam, quieti usu necessarius. Sapienter existimo fruenda contentiones deduceret dolere minuti animi scientia, inbecilloque servire evertunt. Exeduntur vivi laudatum dicitur omnia verterem fuerit inanitate efficiatur</p>
+								<a href="blog_post_modif.php" class="more-link">Modification blog post <span class="meta-nav">&rarr;</span></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+		<footer id="footer"> 
+			<div class="container"> 
+				<div class="text-center"> 
+					<p>Copyright &copy; 2017 - <a href="#">Carole Guardiola</a> | Tous droits réservés</p> 
+					<p style="font-size: 10px;">Site "projet étudiant" réalisé dans le cadre du parcours Développeur d'applications PHP/Symfony d'OpenClassrooms.</p>
+				</div> 
+			</div> 
+		</footer> <!--/#footer--> 
+
+		<a href="#" class="scrollup"><i class="fa fa-angle-up"></i></a>	
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="js/jquery.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery.easing.min.js"></script>
+		<script src="js/jquery.stellar.js"></script>
+		<script src="js/jquery.appear.js"></script>
+		<script src="js/jquery.nicescroll.min.js"></script>
+		<script src="js/jquery.countTo.js"></script>
+		<script src="js/jquery.shuffle.modernizr.js"></script>
+		<script src="js/jquery.shuffle.js"></script>
+		<script src="js/owl.carousel.js"></script>
+		<script src="js/jquery.ajaxchimp.min.js"></script>
+		<script src="js/script.js"></script>
+	</body>
+</html>
