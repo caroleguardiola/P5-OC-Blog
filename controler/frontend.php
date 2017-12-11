4fbbe592c5aa562d0cdaef67f2f468c266827e35
caroleguardiola <carole.guardiola@gmail.com>
2017-12-11 14:31:28 +0100

    Controler frontend modified with Post Object. Name's functions modified. Created new exceptions.

--- controler/frontend.php
+++ controler/frontend.php
@@ -15,50 +15,113 @@ function listPosts()
     require('view/frontend/listPostsView.php');
 }
 
-function post($id)
+function uniquePost($id)
 {
-	$postManager = new PostManager();
+    $postManager = new PostManager();
     $posted = $postManager->getPost($id);
+
+    if (!$posted) {
+        throw new Exception('L\'article demandé n\'existe pas !');
+    }
    
     require('view/frontend/postView.php');
 }
 
-function createPost()
+function createPostPage()
 {
     require('view/frontend/addPostView.php');
 }
 
 function addPost($title, $author, $trailer, $content)
 {
-    $postManager = new PostManager();
-    $addedPost = $postManager->Post($title, $author, $trailer, $content);
+    $errors = [];
 
-   if (empty($_POST['title']) || empty($_POST['author']) || empty($_POST['trailer']) || empty($_POST['content'])) {
-        throw new Exception('Tous les champs ne sont pas remplis !');
-        }
-    else{
+    if (empty($_POST['title']) || !is_string($_POST['title'])) {
+        $errors['title'] = "Vous n'avez pas renseigné le titre ou le titre est invalide !";
+    }
+    if (empty($_POST['author']) || !is_string($_POST['author'])) {
+        $errors['author'] = "Vous n'avez pas renseigné l'auteur ou l'auteur est invalide !";
+    }
+    if (empty($_POST['trailer']) || !is_string($_POST['trailer'])) {
+        $errors['trailer'] = "Vous n'avez pas renseigné le chapô ou le chapô est invalide !";
+    }
+    if (empty($_POST['content']) || !is_string($_POST['content'])) {
+        $errors['content'] = "Vous n'avez pas renseigné le contenu ou le contenu est invalide !";
+    }
+
+    session_start();
+
+    if (!empty($errors)) {
+        $_SESSION['errors'] = $errors;
+        $_SESSION['inputs'] = $_POST;
         header('Location: index.php?action=creationPost');
-        }
-}    
+    }else{
 
-function modifyPost($id)
+    $post = new Post([
+        'title' => $_POST['title'],
+        'author' => $_POST['author'],
+        'trailer' => $_POST['trailer'],
+        'content' => $_POST['content']
+    ]);
+    $postManager = new PostManager();
+    $addedPost = $postManager->addNewPost($post);
+
+    $_SESSION['success'] = 1;
+    
+    header('Location: index.php?action=creationPost');
+    }
+}
+
+function modifyPostPage($id)
 {
     $postManager = new PostManager();
     $posted = $postManager->getPost($id);
+
+    if (!$posted) {
+        throw new Exception('L\'article demandé n\'existe pas !');
+    }
     
     require('view/frontend/updatePostView.php');
 }
 
 function updatePost($id, $title, $author, $trailer, $content)
 {
-    $postManager = new PostManager();
-    $updatedPost = $postManager->modifPost($id, $title, $author, $trailer, $content);
+    $errors = [];
 
-    if (empty($_POST['title']) || empty($_POST['author']) || empty($_POST['trailer']) || empty($_POST['content'])) { 
-        throw new Exception('Tous les champs de modification ne sont pas remplis !');
+    if (empty($_POST['title']) || !is_string($_POST['title'])) {
+        $errors['title'] = "Vous n'avez pas renseigné le titre à modifier ou le titre est invalide !";
     }
-    else {
-        header('Location: index.php?action=post&id=' . $id);
+    if (empty($_POST['author']) || !is_string($_POST['author'])) {
+        $errors['author'] = "Vous n'avez pas renseigné l'auteur à modifier ou l'auteur est invalide !";
+    }
+    if (empty($_POST['trailer']) || !is_string($_POST['trailer'])) {
+        $errors['trailer'] = "Vous n'avez pas renseigné le chapô à modifier ou le chapô est invalide !";
+    }
+    if (empty($_POST['content']) || !is_string($_POST['content'])) {
+        $errors['content'] = "Vous n'avez pas renseigné le contenu à modifier ou le contenu est invalide !";
+    }
+
+    session_start();
+
+    if (!empty($errors)) {
+        $_SESSION['errors'] = $errors;
+        $_SESSION['inputs'] = $_POST;
+        header('Location: index.php?action=modifyPost&id=' . $id);
+    }else{
+
+    $post = new Post([
+        'id' => $_GET['id'],
+        'title' => $_POST['title'],
+        'author' => $_POST['author'],
+        'trailer' => $_POST['trailer'],
+        'content' => $_POST['content']
+    ]);
+    $postManager = new PostManager();
+    $updatedPost = $postManager->updatePost($post);
+
+    $_SESSION['success'] = 1;
+    
+    header('Location: index.php?action=modifyPost&id=' . $id);
     }
 }
 
