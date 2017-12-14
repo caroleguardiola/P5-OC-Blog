229c263c908f19b420d66e383ea2bc88e56400eb
caroleguardiola <carole.guardiola@gmail.com>
2017-12-14 10:37:22 +0100

    Added favicon.ico.

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -10,6 +10,7 @@
 		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
 
 		<link rel="icon" type="image/png" href="favicon.png" />
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
 
 		<!-- Bootstrap -->
 		<link href="public/css/bootstrap.min.css" rel="stylesheet">
